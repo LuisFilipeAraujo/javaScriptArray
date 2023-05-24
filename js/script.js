de75b9a6765b9e83a08632df245297f5e2720247
Luis Filipe Alves Araujo <81663096+LuisFilipeAraujo@users.noreply.github.com>
2023-05-24 00:25:46 -0300

    added new input for age and function sendAge as well

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
-const btn = document.querySelector("#send");
+const enviaNome = document.querySelector("#send1");
+const enviaIdade = document.querySelector("#send2");
 
-btn.addEventListener("click", function(e){
-
+enviaNome.addEventListener("click", function(e){
 // evita evitar o formulário
 e.preventDefault();
-
 const name = document.querySelector("#name");
-
 const value = name.value;
-
 console.log(value);
-
 });
+
+
+enviaIdade.addEventListener("click", function(e){
+    // evita evitar o formulário
+    e.preventDefault();
+    const name = document.querySelector("#idade");
+    const value = name.value;
+    console.log(value);
+    });
